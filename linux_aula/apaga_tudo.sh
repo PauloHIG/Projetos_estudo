26c3fa0a816bd980d116a2eb3e05fe838bdcafc1
paulo <pauloito123@gmail.com>
2023-04-13 19:09:45 -0300

    projeto em andamento

--- linux_aula/apaga_tudo.sh
+++ linux_aula/apaga_tudo.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-sudo userdel temporario -r
+
+sudo rm /home/publico -rf;
+sudo rm /home/adm -rf;
+sudo rm /home/ven -rf;
+sudo rm /home/sec -rf;
+
+
+usuarios = ("carlos" "maria" "joao" "debora" "sebastiana" "roberto" "josefina" "amanda" "rogerio")
+for usuario in "${usuarios[@]}";do
+	sudo userdel -r "$usuario"
+done
+
+grupos = ("GRP_ADM" "GRP_VEN" "GRP_SEC")
+for grupo in "${grupos[@]}";do
+	sudo ugroupdel "$grupo"
+done
--- linux_aula/projeto_IaC.sh
+++ linux_aula/projeto_IaC.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
+echo "Criando diretorios"
+sudo mkdir /home/publico
+sudo mkdir /home/adm
+sudo mkdir /home/ven
+sudo mkdir /home/sec
+
+echo "Criando Grupos"
+sudo groupadd GRP_ADM
+sudo groupadd GRP_VEN
+sudo groupadd GRP_SEC
+
+echo "Alterando grupos dos diretorios"
+sudo chgrp GRP_ADM /home/adm
+sudo chgrp GRP_VEN /home/ven
+sudo chgrp GRP_SEC /home/sec
+
+echo "Alterando permissões para os diretorios"
+sudo chmod 777 /home/publico
+sudo chmod 770 /home/adm
+sudo chmod 770 /home/ven
+sudo chmod 770 /home/se
+
 echo "Criando vários usuarios"
-sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash temporario
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash carlos
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash maria
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash joao
+
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash debora
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash sebastiana
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash roberto
+
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash josefina
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash amanda
+sudo useradd -p $(openssl passwd -1 tempo) -m -s /bin/bash rogerio
+
+echo "Colocado cada usuario em seu respectivo grupo"
+sudo usermod -G GRP_ADM carlos maria joao
+sudo usermod -G GRP_VEN debora sebastiana roberto
+sudo usermod -G GRP_SEC josefina amanda rogerio
 
 echo "Colocando expiração para que ele crie uma nova senha"
-sudo passwd temporario -e
-
+#nota, não implementado para que eu pudesse fazer os testes mais rápido
+#passwd temporario -e
