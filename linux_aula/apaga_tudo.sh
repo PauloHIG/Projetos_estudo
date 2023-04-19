40ab2934ab8f0b8586db14da3eeab63740185eb5
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2023-04-19 00:57:23 -0300

    Update apaga_tudo.sh

--- linux_aula/apaga_tudo.sh
+++ linux_aula/apaga_tudo.sh
@@ -4,8 +4,7 @@ sudo rm /home/publico -rf
 sudo rm /home/adm -rf
 sudo rm /home/ven -rf
 sudo rm /home/sec -rf
-
-
+#usando a lista para iterar por vários usuarios, posso criar um arquivo de configuração fácil de manipular no futuro, garantindo economia de tempo e de linhas de código
 usuarios=("carlos" "maria" "joao" "debora" "sebastiana" "roberto" "josefina" "amanda" "rogerio")
 for usuario in "${usuarios[@]}";do
 	sudo userdel -r "$usuario"
