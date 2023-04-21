fbadd2e08d49fd3f15762a6c416f46a079e0dc76
Paulo H. Ito <pauloito123@gmail.com>
2023-04-21 00:43:44 -0300

    aprendendo convenções

--- curso_java/exercicios/teste.java
+++
@@ -1,9 +0,0 @@
-import javax.sound.midi.Soundbank;
-import javax.sound.sampled.SourceDataLine;
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-
-public class teste {
-    public static void main(){
-        System.out.println("teste");
-    }
-}
