eb60781d827f059f537e961f62894dac68ddcb8d
Paulo H. Ito <pauloito123@gmail.com>
2023-04-21 00:10:25 -0300

    deletando e fazendo certo

---
+++ curso_java/exercicios/teste.java
@@ -0,0 +1,9 @@
+import javax.sound.midi.Soundbank;
+import javax.sound.sampled.SourceDataLine;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class teste {
+    public static void main(){
+        System.out.println("teste");
+    }
+}
