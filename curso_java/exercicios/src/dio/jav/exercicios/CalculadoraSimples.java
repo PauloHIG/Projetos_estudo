52ef183b5f5beee93624f34d1380befda7599a81
Paulo H. Ito <pauloito123@gmail.com>
2023-04-23 00:29:23 -0300

    mais arquivos

--- curso_java/exercicios/src/dio/jav/exercicios/CalculadoraSimples.java
+++ curso_java/exercicios/src/dio/jav/exercicios/CalculadoraSimples.java
@@ -1,10 +1,25 @@
 package dio.jav.exercicios;
-
+/**
+ * @author Paulo Henrique Ito
+ * @since 22/04/2023
+ */
 public class CalculadoraSimples {
     //para treinar criação de métodos
+    /**
+     * @author Paulo H. Ito
+     * @param a primeiro número
+     * @param b segundo número
+     * @return soma entre esses dois números
+     */
     public static int somar(int a,int b) {
         return a+b;
     }
+    /**
+     * 
+     * @param n1 o número que vai sofrer a subtração
+     * @param n2 o número que vai subtrair
+     * @return o resultado da operação
+     */
     public static int subtrair(int n1,int n2){
         return n1-n2;
     }
@@ -17,11 +32,10 @@ public class CalculadoraSimples {
     public static void main(String[] args) {
         //deixei essa instancia que eu fiz para nunca esquecer o que o static faz, tira a necessidade de instancia
         //CalculadoraSimples calculadora = new CalculadoraSimples();
-        int soma = CalculadoraSimples.somar(1,1);
+        int soma = somar(1,1);
         System.out.println(soma);
 
-        System.out.println(CalculadoraSimples.multiplicar(3, 3));
-        System.out.println(CalculadoraSimples.dividir(6, 3));
+        System.out.println(multiplicar(3, 3));
+        System.out.println(dividir(6, 3));
     }
-
 }
--- curso_java/exercicios/src/dio/jav/exercicios/MediaClassica.java
+++ curso_java/exercicios/src/dio/jav/exercicios/MediaClassica.java
@@ -2,8 +2,10 @@ package dio.jav.exercicios;
 
 public class MediaClassica {
     public static void main(String[] args) {
-        int notaFinal = 5;
-        if(notaFinal>=6)
+        //consegui usar os métodos de outra classe, sem instanciar
+        int somaNotas = CalculadoraSimples.somar(6, 3);
+        double media = CalculadoraSimples.dividir(somaNotas, 2); 
+        if(media>=6)
             System.out.println("Aprovado");
         else
             System.out.println("Reprovado");
