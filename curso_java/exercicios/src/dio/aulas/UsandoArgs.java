1006ab595b83f2827d3c1a53b07243c061ce801a
Paulo H. Ito <pauloito123@gmail.com>
2023-04-23 01:44:46 -0300

    aula de argumentos

---
+++ curso_java/exercicios/src/dio/aulas/UsandoArgs.java
@@ -0,0 +1,10 @@
+package dio.aulas;
+
+public class UsandoArgs {
+    public static void main(String[] args) {
+        // argumentos inseridos pelo arquivo de json na pasta .vscode
+        System.out.println("Nome: "+args[0]);
+        System.out.println("Idade: "+ args[1]);
+        System.out.println("Peso: "+args[2]);
+    }
+}
