1dabb1d99cd222b68b5792a64a654871887cab3d
Paulo H. Ito <pauloito123@gmail.com>
2023-05-12 23:51:52 -0300

    fonte

--- curso_java/exercicios/src/dio/exercicios/TestaAPIBuscador.java
+++ curso_java/exercicios/src/dio/exercicios/TestaAPIBuscador.java
@@ -1,5 +1,6 @@
 package dio.exercicios;
 public class TestaAPIBuscador {
+    //https://arthur-almeida.medium.com/consumindo-uma-api-de-maneira-simples-com-java-2a386010e4b9
     public static void main(String[] args) throws Exception {
         Endereco minhaCasa = APIdoServicoDeCep.buscaEnderecoPeloCep("04950140");
         System.out.println(minhaCasa);
