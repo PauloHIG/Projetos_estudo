33b9aa60460539f69907bbee1a7e009bab0c908d
Paulo H. Ito <pauloito123@gmail.com>
2023-05-12 23:51:30 -0300

    encurtando código

--- curso_java/exercicios/src/dio/exercicios/TestaAPIBuscador.java
+++ curso_java/exercicios/src/dio/exercicios/TestaAPIBuscador.java
@@ -1,13 +1,7 @@
 package dio.exercicios;
 public class TestaAPIBuscador {
-    public static void main(String[] args) {
-        try {
-            Endereco minhaCasa = APIdoServicoDeCep.buscaEnderecoPeloCep("04950000");
-            System.out.println(minhaCasa);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
+    public static void main(String[] args) throws Exception {
+        Endereco minhaCasa = APIdoServicoDeCep.buscaEnderecoPeloCep("04950140");
+        System.out.println(minhaCasa);
     }
 }
