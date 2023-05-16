8d34497154853d68ca10fe4bd8eabaed8dd8f712
Paulo H. Ito <pauloito123@gmail.com>
2023-05-16 16:01:35 -0300

    tudo dando errado

--- kotlin_aulas/dio.aulas/src/main/kotlin/dio/apirest/dio/aulas/Application.kt
+++ kotlin_aulas/dio.aulas/src/main/kotlin/dio/apirest/dio/aulas/Application.kt
@@ -1,11 +1,11 @@
-package dio.paulo.cursokotlin.credit
+package dio.apirest.dio.aulas
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CursoKotlinCreditApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<CursoKotlinCreditApplication>(*args)
+	runApplication<Application>(*args)
 }
--- kotlin_aulas/dio.aulas/src/test/kotlin/dio/apirest/dio/aulas/ApplicationTests.kt
+++ kotlin_aulas/dio.aulas/src/test/kotlin/dio/apirest/dio/aulas/ApplicationTests.kt
@@ -1,10 +1,10 @@
-package dio.paulo.cursokotlin.credit
+package dio.apirest.dio.aulas
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class CursoKotlinCreditApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
