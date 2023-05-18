d536ac1c7bc681361353aca2fb26167b4eb2e0fc
Paulo H. Ito <pauloito123@gmail.com>
2023-05-18 20:37:55 -0300

    vivendo e aprendendo

--- curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/Application.java
+++ curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/Application.java
@@ -3,14 +3,12 @@ package dio.paulo.aula.dio.credit.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import dio.paulo.aula.dio.credit.api.dominio.Customer;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		
 	}
 
 }
