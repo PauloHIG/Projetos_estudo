aa0f2c90f8024294816100c14ecf7143ea71a7d2
Paulo H. Ito <pauloito123@gmail.com>
2023-05-18 02:31:23 -0300

    usando o java por enquanto

--- curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/Application.java
+++ curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/Application.java
@@ -3,11 +3,14 @@ package dio.paulo.aula.dio.credit.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import dio.paulo.aula.dio.credit.api.dominio.Customer;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
 	}
 
 }
--- curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/dominio/Credit.java
+++ curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/dominio/Credit.java
@@ -2,9 +2,10 @@ package dio.paulo.aula.dio.credit.api.dominio;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Credit {
-    private UUID creditCode;
+    private UUID creditCode = UUID.randomUUID();
     private BigDecimal creditValue;
     private LocalDate dayFirstInstallment;
     private int numberOfInstallments;
