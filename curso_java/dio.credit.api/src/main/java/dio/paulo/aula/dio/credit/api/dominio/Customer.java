61d9b938970b3106f464bb85072989e19189698d
Paulo H. Ito <pauloito123@gmail.com>
2023-05-18 17:59:53 -0300

    aula

--- curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/dominio/Customer.java
+++ curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/dominio/Customer.java
@@ -2,14 +2,31 @@ package dio.paulo.aula.dio.credit.api.dominio;
 import java.math.BigDecimal;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Cliente")
 public class Customer {
-    private String name;
-    private String lastName;
-    private String cpf;
-    private String email;
-    private String password;
-    private Address adress;
-    private BigDecimal income;
-    private List<Credit> credits;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false) private String name;
+    @Column(nullable = false) private String lastName;
+    @Column(nullable = false,unique = true) private String cpf;
+    @Column(nullable = false,unique = true) private String email;
+    @Column(nullable = false) private String password;
+
+
+    @Embedded @OneToOne(optional = false) private Address adress;
+    @Column(nullable = true) private BigDecimal income;
+    @OneToMany() private List<Credit> credits;
 }
