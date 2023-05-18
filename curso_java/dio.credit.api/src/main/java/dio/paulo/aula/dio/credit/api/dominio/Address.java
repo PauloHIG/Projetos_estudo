9b4f41722d74e07e6780aadb48382af4324fa471
Paulo H. Ito <pauloito123@gmail.com>
2023-05-18 17:59:37 -0300

    assistindo aulas

--- curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/dominio/Address.java
+++ curso_java/dio.credit.api/src/main/java/dio/paulo/aula/dio/credit/api/dominio/Address.java
@@ -1,5 +1,8 @@
 package dio.paulo.aula.dio.credit.api.dominio;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public class Address {
     private String zipCode;//equivalente ao nosso CEP
     private String street;
