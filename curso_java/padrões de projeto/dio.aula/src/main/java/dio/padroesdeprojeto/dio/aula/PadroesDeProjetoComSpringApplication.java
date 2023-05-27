26ec37f49e6c71fad2d5c8252264cc1e5a571dc0
Paulo H. Ito <pauloito123@gmail.com>
2023-05-27 00:43:19 -0300

    read

---
+++ curso_java/padrões de projeto/dio.aula/src/main/java/dio/padroesdeprojeto/dio/aula/PadroesDeProjetoComSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.padroesdeprojeto.dio.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesDeProjetoComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesDeProjetoComSpringApplication.class, args);
+	}
+
+}
---
+++ curso_java/padrões de projeto/dio.aula/src/test/java/dio/padroesdeprojeto/dio/aula/PadroesDeProjetoComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.padroesdeprojeto.dio.aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesDeProjetoComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
