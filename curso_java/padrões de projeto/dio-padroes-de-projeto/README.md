5602327da04fb053894d0360531627235beac9f4
Paulo H. Ito <pauloito123@gmail.com>
2023-05-27 01:25:43 -0300

    criando a classe endereço

---
+++ curso_java/padrões de projeto/dio-padroes-de-projeto/README.md
@@ -0,0 +1,5 @@
+### Explorando Padrões de Projetos na Prática com Java
+Copiei e testei cada exemplo da aula, refiz o getInstance do SingletonLazy usando um operador ternario para testar.
+
+
+[Este artigo](https://medium.com/collabcode/strategy-padr%C3%B5es-de-projeto-em-java-43889a3afc5a) me ajudou a entender strategy
---
+++ curso_java/padrões de projeto/dio.aula/src/main/model/Endereco.java
@@ -0,0 +1,62 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+    @Id
+    String cep;
+    String logradouro;
+    String complemento;
+    String bairro;
+    String localidade;
+    String uf;
+    String ibge;
+    String gia;
+    String ddd;
+    String siafi;
+
+    // Construtor vazio necessário para o Spring
+    public Endereco() {
+    }
+
+    // Getters
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getIbge() {
+        return ibge;
+    }
+
+    public String getGia() {
+        return gia;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public String getSiafi() {
+        return siafi;
+    }
+}
