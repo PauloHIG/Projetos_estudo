8aed2ed18cac14cc2fedd03f4c0cddc7c3ae4718
Paulo H. Ito <pauloito123@gmail.com>
2023-05-30 18:57:01 -0300

    adicionando camada de serviço

---
+++ curso_java/padrões de projeto/dio.aula/src/main/java/dio/padroesdeprojeto/dio/aula/service/implement/ImplClienteService.java
@@ -0,0 +1,40 @@
+package dio.padroesdeprojeto.dio.aula.service.implement;
+
+import java.util.List;
+
+import dio.padroesdeprojeto.dio.aula.model.Cliente;
+import dio.padroesdeprojeto.dio.aula.service.IClienteService;
+
+public class ImplClienteService implements IClienteService {
+
+    @Override
+    public void inserir(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+    }
+
+    @Override
+    public List<Cliente> buscarTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarTodos'");
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarPorId'");
+    }
+
+    @Override
+    public void atualizar(long id, Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    @Override
+    public void deletar(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletar'");
+    }
+    
+}
