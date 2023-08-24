d70aef30900dcea684ae99725af31aa358e8a724
Paulo H. Ito <pauloito123@gmail.com>
2023-08-24 19:18:54 -0300

    hora de voltar

--- curso_java/padrões de projeto/dio.aula/src/main/java/dio/padroesdeprojeto/dio/aula/controller/ClienteController.java
+++ curso_java/padrões de projeto/dio.aula/src/main/java/dio/padroesdeprojeto/dio/aula/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package dio.padroesdeprojeto.dio.aula.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
